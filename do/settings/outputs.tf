0fcabea8bcdc04abf5c34a125792b48bcbf9108f
mangomagic <robert@manogmagic.co.uk>
2023-12-20 18:50:44 +0700

    MM-29: feat, expose more output variables

--- do/settings/outputs.tf
+++ do/settings/outputs.tf
@@ -11,6 +11,16 @@ output "web_project" {
   value = local.web_project
 }
 
+# The environment short name e.g. dev, stage, prod 
 output "environment_slug" {
   value = local.environment_slug
+}
+
+# alias of environment_slug
+output "env" {
+  value = local.environment_slug
+}
+
+output "region" {
+  value = local.region
 }
